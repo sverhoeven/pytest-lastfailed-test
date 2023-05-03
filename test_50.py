8b219fd825ddec609352eec104e4115a616a0f7a
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-05-03 13:36:11 +0200

    more tests

--- test_50.py
+++ test_50.py
@@ -2,7 +2,7 @@ import pytest
 
 # hello
 
-@pytest.mark.parametrize("i", range(51))
+@pytest.mark.parametrize("i", range(52))
 def test_num(i):
     if i in (17, 25):
         pytest.fail("bad luck")
